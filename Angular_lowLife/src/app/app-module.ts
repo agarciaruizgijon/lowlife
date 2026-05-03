68c65059352e5d70adcdd7cdc3edac9a441f242c
vmarinruizgijon <victor.marin-manzanares@iesruizgijon.com>
2026-05-03 21:37:20 +0200

    Creacion del estilos login

--- Angular_lowLife/src/app/app-module.ts
+++ Angular_lowLife/src/app/app-module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { StyleGuide } from './style-guide/style-guide';
+import { Login } from './auth/login/login';
 
 @NgModule({
-  declarations: [App, StyleGuide],
+  declarations: [App, StyleGuide, Login],
   imports: [BrowserModule, AppRoutingModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
--- Angular_lowLife/src/app/app-routing-module.ts
+++ Angular_lowLife/src/app/app-routing-module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { StyleGuide } from './style-guide/style-guide';
+import { Login } from './auth/login/login';
 
 const routes: Routes = [
   { path: 'guia-estilos', component: StyleGuide },
+  { path: 'login', component: Login },
   { path: '', redirectTo: '/guia-estilos', pathMatch: 'full' }
 ];
 
